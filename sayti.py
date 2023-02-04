279b7a41d7af39cc566a600082dcbc1a66ed36eb
SluvEk <slava_lad1234@mail.ru>
2023-02-04 21:53:35 +0300

    Изначальная версия

---
+++ sayti.py
@@ -0,0 +1,18 @@
+import os
+
+urls = [
+    ('https://www.youtube.com/', 'Youtube'),
+    ('https://www.pivo.com/', 'Сайт для пива'),
+    ('https://fentezi.by/', 'Продажа дилдо'),
+    ('https://habr.com/ru/all/', 'Хабр'),
+    ]
+
+url_names = '\n'.join((f' {i + 1}. {url_name}.' for i, (_, url_name) in enumerate(urls)))
+
+slava = input(f'Привет! Что хочешь открыть ?\n{url_names}\nТвой выбор: ')
+
+try:
+    url_num = int(slava) - 1
+    os.system ('start ' + urls[url_num][0])
+except:
+    print('Допустимые значения: 1 -', len(urls))
