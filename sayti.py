439fb1367595fd52238e8764a98775012bd50ba5
SluvEk <slava_lad1234@mail.ru>
2023-02-04 22:24:51 +0300

    зациклили программу

--- sayti.py
+++ sayti.py
@@ -10,13 +10,14 @@ urls = [
 url_names = '\n'.join((f' {i + 1}. {url_name}.' for i, (_, url_name) in enumerate(urls)))
 
 def main():
-    slava = input(f'Привет! Что хочешь открыть ?\n{url_names}\nТвой выбор: ')
+    while True:
+        slava = input(f'Привет! Что хочешь открыть ?\n{url_names}\nТвой выбор: ')
 
-    try:
-        url_num = int(slava) - 1
-        os.system ('start ' + urls[url_num][0])
-    except:
-        print('Допустимые значения: 1 -', len(urls))
+        try:
+            url_num = int(slava) - 1
+            os.system ('start ' + urls[url_num][0])
+        except:
+            print('Допустимые значения: 1 -', len(urls))
 
 if __name__ == '__main__':
     main()
